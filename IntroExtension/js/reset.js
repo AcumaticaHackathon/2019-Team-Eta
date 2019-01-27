c16abecf6099f67520bc37126cf3408fbe0a48e9
Michal Adda <michala@core-assoc.com>
2019-01-27 10:48:19 -0500

    resetting location

--- IntroExtension/js/reset.js
+++ IntroExtension/js/reset.js
@@ -4,7 +4,9 @@ chrome.storage.sync.get(['key'], function (result) {
     x.setRequestHeader('Content-Type', 'application/json')
     x.onload = function() {
         if(x.status === 200) {
-            swal({ title: 'Success', text: '', icon: 'success'});
+            swal({ title: 'Success', text: '', icon: 'success'}).then(result => {
+                window.location.href = "http://localhost/AcumaticaERP/Main?ScreenId=AP3010PL";
+            });
         }
     };
     x.send(JSON.stringify({
