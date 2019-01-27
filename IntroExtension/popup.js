27aa58327054b18a770392fb1ff316d1413b7e51
Simon Ménard-Larivière <slariviere@acumatica.com>
2019-01-26 23:30:38 -0500

    added class tree view

--- IntroExtension/popup.js
+++ IntroExtension/popup.js
@@ -5,3 +5,29 @@ window.addEventListener('load', function load(event) {
           }); 
     };
 });
+
+
+TreeView();
+
+function TreeView() {
+    window.addEventListener('load', function load(event) {
+        var toggler = document.getElementsByClassName("caret");
+        var i;
+
+        for (i = 0; i < toggler.length; i++) {
+            toggler[i].addEventListener("click", function () {
+                this.parentElement.querySelector(".nested").classList.toggle("active");
+                this.classList.toggle("caret-down");
+            });
+        }
+
+        var steps = document.getElementsByClassName("step");
+        var i;
+
+        for (i = 0; i < steps.length; i++) {
+            steps[i].addEventListener("click", function () {
+                window.location.href = this.href;
+            });
+        }
+    });
+}
