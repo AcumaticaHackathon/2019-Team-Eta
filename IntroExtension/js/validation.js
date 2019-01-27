f300aeeb6987a9fb7b307b987be7204a8f0e01c4
Laura Lacatusu <laural@core-assoc.com>
2019-01-27 07:07:55 +0200

    added alert on error

--- IntroExtension/js/validation.js
+++ IntroExtension/js/validation.js
@@ -1,18 +1,15 @@
 chrome.storage.sync.get(['key'], function (result) {
 
-/*    swal({
-        title: "Good job!",
-        text: "You clicked the button!",
-        icon: "success",
-    });*/
-
     var x = new XMLHttpRequest();
     x.open('POST', 'http://localhost:51410/api/validation');
     x.setRequestHeader('Content-Type', 'application/json')
     x.onload = function() {
         if(x.status === 200) {
-            alert('success');
-            //todo laura replace :)
+            swal({ title: 'Success', text: '', icon: 'success'});
+        }
+
+        if(x.status === 400) {
+            swal({ title: '', text: x.responseText, icon: 'error'});
         }
     };
     x.send(JSON.stringify({
